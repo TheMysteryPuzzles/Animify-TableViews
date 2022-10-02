1b3e3dad9c1fb583edd236374fd47336269633d6
Omer Khan <omerkhann024@gmail.com>
2022-10-02 23:55:34 +0500

    Initial Commit

---
+++ Animify/Animify.h
@@ -0,0 +1,18 @@
+//
+//  Animify.h
+//  Animify
+//
+//  Created by Omer Khan on 02/10/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Animify.
+FOUNDATION_EXPORT double AnimifyVersionNumber;
+
+//! Project version string for Animify.
+FOUNDATION_EXPORT const unsigned char AnimifyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Animify/PublicHeader.h>
+
+
